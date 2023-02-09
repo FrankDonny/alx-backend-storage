7f8e26336ef239a1042891b4effb97d45619c00a
frankdonny <arkohfrank1@gmail.com>
2023-02-09 16:55:28 +0000

    0x00. MySQL advanced

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the average score for a student. Note: An average
+-- score can be a decimal
+USE holberton;
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score DECIMAL(10, 2);
+    SELECT AVG(score) INTO avg_score FROM corrections
+        WHERE corrections.user_id = user_id;
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
+END //
+DELIMITER ;
