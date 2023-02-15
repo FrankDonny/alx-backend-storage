81efeace88962c8e5712d5e03f7617168ca1dc66
frankdonny <arkohfrank1@gmail.com>
2023-02-15 02:31:58 +0000

    0x01. NoSQL

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,17 +5,11 @@
 def top_students(mongo_collection):
     """function that returns all students sorted by average score"""
     students = mongo_collection.find({})
-    # print("\nstart\n")
-    # print([student["topics"] for student in students])
-    # print("\nend\n")
-    # return [student for student in students]
-    studentDetail = []
     for student in students:
         sum = 0
         for topic in student['topics']:
             sum += topic['score']
-        studentDetail.append(
-            {"name": student['name'], "avg": sum/len(student['topics'])})
-    studentDetail.sort(key=lambda x: x["avg"])
-    return [studentDetail]
-    # return [student for student in students]
+        mongo_collection.update_one({"name": student['name']}, {
+                                    "$set": {"averageScore":
+                                             sum/len(student['topics'])}})
+    return mongo_collection.find({}).sort("averageScore", -1)
