cfdfc6ea48068f8b34a51058b0fe7fbb478338dd
frankdonny <arkohfrank1@gmail.com>
2023-02-16 02:48:03 +0000

    0x02. Redis basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Optional, Union
 from uuid import uuid4
 
 import redis
@@ -12,28 +12,33 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """store method"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable]) -> Any:
+    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes,
+                                                             int, float, None]:
         """Reading from Redis and recovering original type"""
         value = self._redis.get(key)
-        if value is None:
-            return None
         if fn is not None:
-            value = fn(value)
+            return fn(value)
         return value
 
     def get_str(self, key: str) -> str:
         """calls the get method and converts the key to string"""
-        return self.get(key, lambda x: x.decode('utf-8'))
+        data = self._redis.get(key)
+        return data.decode('utf-8')  # type: ignore
 
     def get_int(self, key: str) -> int:
         """calls the get method and converts the key to int"""
-        return self.get(key, lambda x: int(x))
+        data = self._redis.get(key)
+        try:
+            data = int(data)  # type: ignore
+        except Exception:
+            data = 0
+        return data
 
 
 cache = Cache()
